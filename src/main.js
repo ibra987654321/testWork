0940037105dd1f2bc4004e7e5e5e520bc6c1d9c6
ibra987654321 <57038304+ibra987654321@users.noreply.github.com>
2023-09-05 09:48:55 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueYouTubeEmbed from 'vue-youtube-embed'
 import router from './router/index'
 import store from "@/store";
 
-Vue.use(VueYouTubeEmbed)
 Vue.config.productionTip = false
 
 new Vue({
